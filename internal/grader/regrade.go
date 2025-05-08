4c1ef64fd90f1ad147b3db21aa02b4fe13e43031
Lucas Ellenberger <lmellenb@ucsc.edu>
2025-05-07 17:21:11 -0700

    (wip) Updating regrade to start working with the job manager updates. [skip ci]

--- internal/grader/regrade.go
+++ internal/grader/regrade.go
@@ -7,22 +7,26 @@ import (
 
 	"github.com/edulinq/autograder/internal/config"
 	"github.com/edulinq/autograder/internal/db"
-	"github.com/edulinq/autograder/internal/lockmanager"
+	"github.com/edulinq/autograder/internal/jobmanager"
 	"github.com/edulinq/autograder/internal/log"
 	"github.com/edulinq/autograder/internal/model"
+	"github.com/edulinq/autograder/internal/timestamp"
 	"github.com/edulinq/autograder/internal/util"
 )
 
 type RegradeOptions struct {
+	jobmanager.JobOptions
+
 	Users []string `json:"users"`
 
-	// Wait for the entire regrade to complete and return all results.
-	WaitForCompletion bool `json:"wait-for-completion"`
+	// Ensure all users have a new submission after this time.
+	// If there is not a submission after this time,
+	// the user's most recent submission will be regraded.
+	// A value of zero will default to the current time.
+	After timestamp.Timestamp `json:"after"`
 
 	Options GradeOptions `json:"-"`
 
-	// A context that can be used to cancel the regrade.
-	Context    context.Context   `json:"-"`
 	Assignment *model.Assignment `json:"-"`
 
 	// If true, do not swap the context to the background context when running.
@@ -33,89 +37,49 @@ type RegradeOptions struct {
 }
 
 func RegradeSubmissions(options RegradeOptions) (map[string]*model.SubmissionHistoryItem, int, error) {
-	if options.Context == nil {
+	if !options.RetainOriginalContext && !options.WaitForCompletion {
 		options.Context = context.Background()
 	}
 
-	if options.WaitForCompletion {
-		results, err := runRegradeSubmissions(options)
-		if err != nil {
-			return nil, 0, err
-		}
-
-		return results, 0, nil
-	} else {
-		if !options.RetainOriginalContext {
-			options.Context = context.Background()
-		}
-
-		go func() {
-			_, err := runRegradeSubmissions(options)
-			if err != nil {
-				log.Error("Failure during asynchronous regrading.", err)
-			}
-		}()
-	}
-
-	return map[string]*model.SubmissionHistoryItem{}, len(options.Users), nil
-}
-
-func runRegradeSubmissions(options RegradeOptions) (map[string]*model.SubmissionHistoryItem, error) {
-	if len(options.Users) == 0 {
-		return map[string]*model.SubmissionHistoryItem{}, nil
+	if options.After == 0 {
+		options.After = timestamp.Now()
 	}
 
 	// Lock based on the course to prevent multiple requests using up all the cores.
 	lockKey := fmt.Sprintf("regrade-course-%s", options.Assignment.GetCourse().GetID())
-	lockmanager.Lock(lockKey)
-	defer lockmanager.Unlock(lockKey)
 
-	// The context has been canceled while waiting for a lock, abandon this regrade.
-	if options.Context.Err() != nil {
-		return nil, nil
+	job := jobmanager.Job[string, *model.SubmissionHistoryItem]{
+		JobOptions:              &options.JobOptions,
+		LockKey:                 lockKey,
+		PoolSize:                config.REGRADE_COURSE_POOL_SIZE.Get(),
+		ReturnIncompleteResults: !options.WaitForCompletion,
+		WorkItems:               options.Users,
+		// TODO: Need a course get recent submission history that takes a list of users.
+		RetrieveFunc: func(users []string) (map[string]*model.SubmissionHistoryItem, error) {
+			return db.GetRecentSubmissionSurvey(options.Assignment, users)
+		},
+		WorkFunc: func(user string) (*model.SubmissionHistoryItem, error) {
+			return runRegrade(options, user)
+		},
+		WorkItemKeyFunc: func(user string) string {
+			return fmt.Sprintf("%s-%s", lockKey, user)
+		},
 	}
 
-	results := make(map[string]*model.SubmissionHistoryItem, len(options.Users))
-
-	poolSize := config.REGRADE_COURSE_POOL_SIZE.Get()
-	type PoolResult struct {
-		User   string
-		Result *model.SubmissionHistoryItem
-		Error  error
-	}
-
-	poolResults, _, err := util.RunParallelPoolMap(poolSize, options.Users, options.Context, func(user string) (PoolResult, error) {
-		result, err := runSingleRegrade(options, user)
-		if err != nil {
-			err = fmt.Errorf("Failed to perform individual regrade for user %s: '%w'.", user, err)
-		}
-
-		return PoolResult{user, result, err}, nil
-	})
-
+	err := job.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("Failed to run parallel pool map: '%w'.", err)
+		return nil, 0, fmt.Errorf("Failed to validate job: '%v'.", err)
 	}
 
-	// If the regrade was canceled, exit right away.
-	if options.Context.Err() != nil {
-		return nil, nil
+	output := job.Run()
+	if output.Error != nil {
+		return nil, 0, fmt.Errorf("Failed to run regrade job with '%d' work errors: '%v'.", len(output.WorkErrors), output.Error)
 	}
 
-	var errs error = nil
-
-	for _, poolResult := range poolResults {
-		if poolResult.Error != nil {
-			errs = errors.Join(errs, poolResult.Error)
-		} else {
-			results[poolResult.User] = poolResult.Result
-		}
-	}
-
-	return results, errs
+	return output.ResultItems, len(output.RemainingItems), nil
 }
 
-func runSingleRegrade(options RegradeOptions, user string) (*model.SubmissionHistoryItem, error) {
+func runRegrade(options RegradeOptions, user string) (*model.SubmissionHistoryItem, error) {
 	previousResult, err := db.GetSubmissionContents(options.Assignment, user, "")
 	if err != nil {
 		return nil, fmt.Errorf("Failed to get most recent grading result for '%s': '%w'.", user, err)
